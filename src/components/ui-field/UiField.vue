5f386b10a826122c7e87e966ac24ccea209d887f
Aleksey Stepanov <aleksey.s@devstark.com>
2023-07-09 11:36:17 +0300

    add ui-components

---
+++ src/components/ui-field/UiField.vue
@@ -0,0 +1,49 @@
+<template>
+  <component :is="component" :class="classList">
+    <slot></slot>
+  </component>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+// props
+const props = defineProps({
+  component: {
+    type: String,
+    default: 'div'
+  },
+  display: {
+    type: String,
+    default: 'flex'
+  },
+  fullWidth: {
+    type: Boolean,
+    default: false
+  },
+  fullHeight: {
+    type: Boolean,
+    default: false
+  },
+  direction: {
+    type: String,
+    default: 'row'
+  },
+  justify: {
+    type: String,
+    default: 'center'
+  },
+  align: {
+    type: String,
+    default: 'center'
+  }
+})
+
+// computed
+const classList = computed(
+  () =>
+    `${props.display} ${props.fullHeight ? 'full-height' : ''} ${
+      props.fullWidth ? 'full-width' : ''
+    } flex-${props.direction} justify-${props.justify} items-${props.align}`
+)
+</script>
---
+++ src/components/ui-section/UiSection.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="[display, direction, justify, align, item]">
+    <slot ></slot>
+  </div>
+</template>
+<script setup>
+
+// props
+defineProps({
+  display: {
+    type: String,
+    default: 'flex'
+  },
+  direction: {
+    type: String,
+    default: 'row'
+  },
+  justify: {
+    type: String,
+    default: 'justify-center'
+  },
+  align: {
+    type: String,
+    default: 'align-start'
+  },
+  item: {
+    type: String,
+    default: 'item-start'
+  }
+})
+
+</script>
