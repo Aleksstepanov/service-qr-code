9fdb082b2ee18a01532113d8e6e6a4ac43804b65
Aleksey Stepanov <stepanovl87@gmail.com>
2023-08-03 22:35:20 +0300

    fix queries me and add component page-preloaddr

--- src/_apis_/user.js
+++ src/_apis_/user.js
@@ -6,6 +6,14 @@ const user = {
   email: 'admin@test.com',
   password: '123qwe'
 }
+const person = {
+  email: 'admin@test.com',
+  role: 'admin',
+  avatar: '/admin_avatar.jpg',
+  first_name: 'John',
+  last_name: 'Doe',
+  text: 'lorem ipsum'
+}
 
 mock.onPost('/api/account/sign-in').reply(async (config) => {
   try {
@@ -14,19 +22,22 @@ mock.onPost('/api/account/sign-in').reply(async (config) => {
     if (user.email === email && user.password === password) {
       return [200, {
         user: {
-          email,
-          role: 'admin',
-          avatar: '/admin_avatar.jpg',
-          first_name: 'John',
-          last_name: 'Doe',
-          text: 'lorem ipsum'
-
+          ...person
         }
       }]
     }
     return [401, { message: 'Неправильный логин и/или пароль', code: 401 }]
   } catch (error) {
     console.error(error)
+    return [500, { message: 'Ошибка сервера', code: 500 }]
+  }
+})
+
+mock.onGet('/api/account/me').reply(async () => {
+  try {
+    await fakeRequest(2000)
+    return [200, { user: person }]
+  } catch (error) {
     return [500, { message: 'Ошибка сервера', code: 500 }]
   }
 })
