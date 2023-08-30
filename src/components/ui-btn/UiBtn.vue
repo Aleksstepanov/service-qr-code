b75bbee70a041b1888651b48f5c7733c9a1b5ce1
Aleksey Stepanov <alekse.s@devstark.com>
2023-08-30 20:31:10 +0300

    review

--- src/components/ui-btn/UiBtn.vue
+++ src/components/ui-btn/UiBtn.vue
@@ -1,19 +1,22 @@
 <template>
-  <q-btn v-bind="$attrs"
-         :v-close-popup="vClosePopup"
-         :to="to"
-         :disabled ="disabled"
-         :flat="flat"
-         :round="round"
-         :mask="mask"
-         :color="color"
-         :icon="icon"
-         :type="type"
-         :size="size"
-         @click="type === 'submit' ? $emit('submit') : $emit('click')"
-  ><slot></slot>
-    <span v-if="label">
-      {{ label }}</span>
+  <q-btn
+    v-bind="$attrs"
+    :v-close-popup="vClosePopup"
+    :to="to"
+    :disabled="disabled"
+    :flat="flat"
+    :round="round"
+    :mask="mask"
+    :color="color"
+    :icon="icon"
+    :type="type"
+    :size="size"
+    @click="type === 'submit' ? $emit('submit') : $emit('click')"
+  >
+
+    <slot></slot>
+    <!-- span давай уберем -->
+    <span v-if="label"> {{ label }}</span>
   </q-btn>
 </template>
 <script setup>
@@ -21,13 +24,13 @@
 
 // props
 defineProps({
-
   vClosePopup: {
     type: Boolean,
     default: false
   },
   to: {
     type: [String, Object],
+    // @review required удалить
     requred: false,
     default: null
   },
@@ -41,6 +44,7 @@ defineProps({
   },
   size: {
     type: String,
+    // @review лучше по дефолту что то передать
     default: ''
   },
   label: {
@@ -48,11 +52,13 @@ defineProps({
     default: ''
   },
   mask: {
+    // @review такого пропа я не нашел
     type: String,
     default: ''
   },
   color: {
     type: String,
+    // @review лучше по дефолту что то передать
     default: ''
   },
   flat: {
@@ -61,6 +67,7 @@ defineProps({
   },
   icon: {
     type: String,
+    // @review required удалить
     requred: false,
     default: ''
   },
@@ -69,17 +76,24 @@ defineProps({
     default: false
   },
   round: {
+    // @review не думаю, что нам этот проп вообще нужен будет
     type: Boolean,
     default: false
   },
   push: {
+    // @review не думаю, что нам этот проп вообще нужен будет
     type: Boolean,
     default: false
   }
-
 })
 
 // emits
+/*
+@review
+теперь про обработку событий.
+Если передан submit, то передаем submit
+Если type === button, то передаем click
+Если передан проп to, то обработчик ничего делать не должен
+*/
 const $emit = defineEmits(['click'])
-
 </script>
